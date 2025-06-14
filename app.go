4ea2f8ee579be28863181836578468f4126d17b6
KAUSTAVROY979 <kaustavroy979@gmail.com>
2025-06-14 18:20:11 +0530

    Learning functions and return types

--- app.go
+++ app.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello World")
-	one()
+	fmt.Print("Hello World\n")
+	fmt.Print("Your return would be: ", investment_calculator())
+}
+
+func investment_calculator() float64 {
+	var investedAmount = 1000
+	var returnRate = 5.5
+	var years = 10
+
+	return float64(investedAmount) + math.Pow(1+returnRate/100, float64(years))
+
 }
